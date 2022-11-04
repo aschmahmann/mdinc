03e91255b1e44d6da6544e8473e99837fc4eb4db
Adin Schmahmann <adin.schmahmann@gmail.com>
2022-11-04 03:00:37 -0400

    vendor modules

---
+++ vendor/github.com/ipld/edelweiss/defs/service.go
@@ -0,0 +1,26 @@
+package defs
+
+// Service is a set of named functions.
+type Service struct {
+	Methods Methods
+}
+
+func (Service) Kind() string {
+	return "Service"
+}
+
+type Method struct {
+	Name     string
+	Type     Fn
+	Cachable bool
+}
+
+type Methods []Method
+
+func (m Method) Call() Call {
+	return Call{ID: String{}, Fn: m.Type}
+}
+
+func (m Method) Return() Return {
+	return Return{ID: String{}, Fn: m.Type}
+}
